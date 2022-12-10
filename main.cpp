e3367334110b79b71ca44b16206f758352d1ab7a
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-12-10 22:30:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main()
+{
+    int n=0;
+    cin >> n;
+    float a[n];
+    for (int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    cout << array(n, a) << endl;
+    return 0;
 }
