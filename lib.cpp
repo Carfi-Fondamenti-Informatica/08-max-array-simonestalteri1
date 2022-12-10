e76067132dcc4d6482112127530ba99eb6742794
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-12-10 22:28:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+//
+// Created by Hp on 09/12/2022.
+//
 
+#include "lib.h"
+
+float array (int n, float a[])
+{
+    for(int i=0; i<n; i++)
+    {
+        if(a[0] <= a[i])
+        {
+            a[0] = a[i];
+        }
+        else
+        {
+            a[i] = a[0];
+        }
+    }
+    return a[0];
+}
